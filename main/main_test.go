afd2394dfb7affbb8501e66533aac65a52ec089c
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-04-22 00:28:31 +0700

    GO-ROUTINESS: Added and learned how channel works on a loop

--- main/main_test.go
+++ main/main_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestChannelWaitWhenEmpty(t *testing.T) {
+	// channels by default are pass by reference, even when passing to a function
 	ch := make(chan int)
 
 	go func() {
@@ -14,9 +15,11 @@ func TestChannelWaitWhenEmpty(t *testing.T) {
 		close(ch)
 	}()
 
+	// for range loop will wait until channel is closed, so it'll be blocked here
 	for value := range ch {
 		fmt.Println("Received:", value)
 	}
 
+	// this will wait until channel is closed
 	fmt.Println("Waitin indefinitely...")
 }
